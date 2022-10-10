bf3f1f7e5afc9b72cc7301f4e22b9a59448fd011
Michael SN <michael.dev.nascimento@gmail.com>
2022-10-10 16:30:18 -0300

    feat[Product]: product get

---
+++ wp-content/themes/ranekApi/endpoints/product_get.php
@@ -0,0 +1,55 @@
+<?php
+
+function api_product_get($request)
+{
+  $slug = $request["slug"];
+  $post_id = get_product_id_by_slug($slug);
+
+  if ($post_id) {
+
+    $post_meta = get_post_meta($post_id);
+
+
+    $images_array = null;
+    $images = get_attached_media('image', $post_id);
+
+    if ($images) {
+      $images_array = array();
+
+      foreach ($images as $key => $value) {
+        $images_array[] = array(
+          'title' => $value->post_name,
+          'src' => $value->guid,
+        );
+      }
+    }
+
+    $response = array(
+      'id' => $slug,
+      'photos' => $images_array,
+      'name' => $post_meta['name'][0],
+      'price' => $post_meta['price'][0],
+      'description' => $post_meta['description'][0],
+      'sold' => $post_meta['sold'][0],
+      'user_id' => $post_meta['user_id'][0],
+    );
+  } else {
+    $response = new WP_Error('permission', 'Produto não encontrado', array('status', 404));
+  }
+
+  return $response;
+}
+
+
+function register_api_product_get()
+{
+  register_rest_route('api', '/product/(?P<slug>[-\w]+)', array(
+    array(
+      'methods' => WP_REST_Server::READABLE,
+      'callback' => 'api_product_get'
+    )
+  ));
+};
+
+
+add_action('rest_api_init', 'register_api_product_get');
--- wp-content/themes/ranekApi/functions.php
+++ wp-content/themes/ranekApi/functions.php
@@ -4,15 +4,33 @@ $template_directory = get_template_directory();
 
 require_once($template_directory . "/custom-post-type/product.php");
 require_once($template_directory . "/custom-post-type/transaction.php");
-
+// Usuario
 require_once($template_directory . "/endpoints/user_post.php");
 require_once($template_directory . "/endpoints/user_get.php");
 require_once($template_directory . "/endpoints/user_put.php");
+// Produto
+require_once($template_directory . "/endpoints/product_post.php");
+require_once($template_directory . "/endpoints/product_get.php");
+
+
+function get_product_id_by_slug($slug)
+{
+  $query = new WP_Query(array(
+    'name' => $slug,
+    'post_type' => 'product',
+    'numberposts' => 1,
+    'fields' => 'ids',
+  ));
+
+  $posts = $query->get_posts();
+
+  return array_shift($posts);
+}
 
 
 function expire_token()
 {
-  return time() * 86400;
+  return time() * 86400; // 60 * 60 * 24 = 86400
 }
 
 add_action('jwt_auth_expire', 'expire_token');
